8be7e62ec04db99fc377de01a6ffba0c4085a524
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-06-03 00:14:30 +0530

    making it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 
 
 app.get('/', (req,res) => {
-	res.send(database.users);
+	res.send('it is working');
 })
 
 app.post('/signin', (req,res) => { signin.handleSignin(req,res,db,bcrypt) })
