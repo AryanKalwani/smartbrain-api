b80e61ab38a5ef41c80854fec851f64fd8138a3a
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-06-03 14:21:31 +0530

    updating register

--- controllers/register.js
+++ controllers/register.js
@@ -15,16 +15,16 @@ const handleRegister = (req,res,db,bcrypt) => {
 				return trx('users')
 					.returning('*')
 					.insert({
-					email: loginEmail[0],
-					name: name,
-					joined: new Date()
-				})
-				.then(user => {
-					res.json(user[0]);
-				})
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						res.json(user[0]);
+					})
 			})
-		.then(trx.commit)
-		.catch(trx.rollback)
+			.then(trx.commit)
+			.catch(trx.rollback)
 		})
 		.catch(err=>res.status(400).json('unable to register'));
 }
