46faf168356b0a6bdb44501b3a21337a8e9bd78d
AryanKalwani <50944036+AryanKalwani@users.noreply.github.com>
2020-06-20 15:38:22 +0530

    Changing clarifai model

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req,res) => {
 	app.models
-		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.predict(Clarifai.DEMOGRAPHICS_MODEL, req.body.input)
 		.then(data => {
 			res.json(data);
 		})
